09500618641602265792f39984a7d01962dd1558
Raju Sheoran <91339861+rajusheoran4@users.noreply.github.com>
2022-03-22 13:40:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+console.log('This is tutorial 21 ')
+
+// Creating a div with javascript
+let divElem = document.createElement('div');
+
+// Styling the created div
+divElem.setAttribute('id', 'elem');
+divElem.setAttribute('class', 'elem');
+divElem.setAttribute('style', 'border: 2px solid #fff; margin:23px; padding:23px; width:auto;overflow:hidden;');
+
+
+let val = localStorage.getItem('text');
+let text;
+if (val == null) {
+
+    text = document.createTextNode('This is my editable div. Click to edit it.');
+}
+else {
+    text = document.createTextNode(val)
+}
+
+// Insert created text into editable div
+divElem.appendChild(text);
+
+// Grab the cotainer 
+let container = document.querySelector('.container');
+let first = document.getElementById('myfirst');
+
+
+// Insert the created div
+container.insertBefore(divElem, first)
+
+
+// Adding Click and Blur Event Listner on the editable div
+divElem.addEventListener('click', function () {
+    let noTextArea = document.getElementsByClassName('textarea').length;
+    if (noTextArea == 0) {
+        let html = elem.innerHTML;
+        divElem.innerHTML = `<textarea class="textarea form-control" id="textarea" rows="3">${html}</textarea>`;
+    }
+    let textarea = document.getElementById('textarea');
+    textarea.addEventListener('blur', function () {
+        elem.innerHTML = textarea.value;
+        localStorage.setItem('text', textarea.value);
+    })
+})
+
+// Fire the console to check error
+console.log(divElem, container, first);
